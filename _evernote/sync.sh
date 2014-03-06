dca76aa98639ccdbe68d8e434850677f876b7850
Sangmin Yoon <sangmin.yoon@croquis.com>
2014-03-06 15:19:26 +0900

    setup blog

---
+++ _evernote/sync.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git pull origin master
+
+rm ../_posts/evernote/*.html
+mkdir -p ../_posts/evernote
+mkdir -p ../assets/evernote
+
+coffee fetch.coffee
+
+git add ../_posts
+git commit -m 'update posts from Evernote'
+
+git push origin master
+
+cd ..
+./_deploy.sh
